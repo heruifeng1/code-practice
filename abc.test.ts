6e05ec878cf9daecf07aa223b43d63dbfbe6b16c
heruifeng <592879803@qq.com>
2020-08-12 21:25:38 +0800

    先让测试通过

--- abc.test.ts
+++ abc.test.ts
@@ -1,9 +1,20 @@
 function sum(a:number,b:number){
   return a+b
 }
-
+class Dollar {
+  amount:number
+  constructor(num:number) {
+    this.amount = num
+  }
+  times(num:number){
+    this.amount = this.amount * num
+  }
+}
 function testMultiplication(){
-  const five = new Dollor(5);
+  const five = new Dollar(5);
   five.times(2);
   expect(five.amount).toEqual(10);
 }
+
+test('sadf',testMultiplication)
+
